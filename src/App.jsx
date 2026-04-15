01b256a61f57fce9a2015de0969ca5d6c429fa4b
Chris Umali <chrisumali.work@gmail.com>
2026-04-15 22:03:14 +0800

    Rendering multidimensional lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Player from "./components/Player";
-
+import GameBoard from "./components/Gameboard";
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
@@ -11,9 +9,10 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
 }
 
-export default App
+export default App;
