6b60ee9497a93e46683e9f956bd9710f8eba921f
Chris Umali <chrisumali.work@gmail.com>
2026-04-15 22:03:41 +0800

    gamboard component

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,19 @@
+const intialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {intialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => <list key={colIndex}><button>{playerSymbol}</button></list> )}
+            </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
